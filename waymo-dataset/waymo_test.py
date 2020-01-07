b331d8550da9dd4af82ada3257a7bc9c339685af
Akito Komatsu <akitrb001@gmail.com>
2020-01-07 14:18:50 +0900

    画像をファイルに出力できるようにした。 あとはTFrecordを自動で読み込んで変換できるようにしたい

--- waymo-dataset/waymo_test.py
+++ waymo-dataset/waymo_test.py
@@ -1,6 +1,7 @@
 """
 TF2.0系で積極実行した場合のみTFreordのイテレーションが可能なのでこのコードを実行する場合は2.0系で実行する
 """
+import sys
 import os
 import imp
 import tensorflow as tf
@@ -12,16 +13,17 @@ from waymo_open_dataset.utils import range_image_utils
 from waymo_open_dataset.utils import transform_utils
 from waymo_open_dataset import dataset_pb2 as open_dataset
 tf.enable_eager_execution()
-def image_show(data, name, layout, cmap=None):
+sys.setrecursionlimit(10000)
+def image_show(data, name, cmap=None):
     """Show an image."""
     #print(type(data))
-    fig = plt.figure()
-    plt.subplot(*layout)
+    plt.figure()
     plt.imshow(tf.image.decode_jpeg(data), cmap=cmap)
-    plt.title(name)
+    #plt.title(name)
     plt.grid(False)
     plt.axis('off')
-    plt.show()
+    plt.savefig("figure/figure{}.png".format(index))
+    #plt.show()
 
 if __name__ == "__main__":
     FILENAME = "F:\waymo-dataset\segment-10206293520369375008_2796_800_2816_800_with_camera_labels.tfrecord"
@@ -33,8 +35,6 @@ if __name__ == "__main__":
         frame.ParseFromString(bytearray(data.numpy()))
         frames.append(frame)
     print(len(frames))
-    plt.figure(figsize=(25, 20))
     frame = frames[100]
-    for index, image in enumerate(frame.images):
-       for index,frame in enumerate(frames):
-          image_show(frame.images[0].image, open_dataset.CameraName.Name.Name(frame.images[0].name)+str(index),[10,20,index+1])
+    for index,frame in enumerate(frames):
+      image_show(frame.images[0].image,index)
