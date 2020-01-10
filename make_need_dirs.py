1eac5b03593ad197260a030e2dad032d2c981f41
Akito Komatsu <akitrb001@gmail.com>
2020-01-10 18:34:26 +0900

    からディレクトリ生成用スクリプト作成

---
+++ make_need_dirs.py
@@ -0,0 +1,7 @@
+import os
+current_dir = os.getcwd()
+os.mkdir(current_dir+"/nnmodel")
+os.mkdir(current_dir+"/masked")
+os.mkdir(current_dir+"/resized")
+os.mkdir(current_dir+"/tb_log")
+os.mkdir(current_dir+"/out")
